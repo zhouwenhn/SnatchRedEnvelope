db8f5fb801276be1753a4815849f9821307807c1
周文 <chowenzhou@gmail.com>
2017-01-12 12:43:39 +0800

    update

--- app/src/main/java/com/chowen/apackage/snatchredenvelope/MainActivity.java
+++ app/src/main/java/com/chowen/apackage/snatchredenvelope/MainActivity.java
@@ -2,16 +2,26 @@ package com.chowen.apackage.snatchredenvelope;
 
 import android.content.Intent;
 import android.provider.Settings;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
 // TODO: 2017/1/11 红包声音
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initDrawerLayout();
 
         findViewById(R.id.btn_access).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +31,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initDrawerLayout() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        ab.openOptionsMenu();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        setupDrawerContent(mNavigationView);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
     }
 }
